c386f08a4849f3034ffd37d8e42eb3aeb453be4e
Chitresh Deshpande <cdeshpande@ebay.com>
2018-02-03 09:40:02 -0800

    added revison version to FirstMissingPositive

--- src/com/algos/practice/leetcode/hard/FirstMissingPositive.java
+++ src/com/algos/practice/leetcode/hard/FirstMissingPositive.java
@@ -6,15 +6,40 @@ package com.algos.practice.leetcode.hard;
 public class FirstMissingPositive {
 
 
+    protected int firstMissingPostiveRevision(int [] nums) {
+        if(nums == null || nums.length == 0)
+            return 1;
+
+        for(int currIndex = 0; currIndex < nums.length; currIndex++) {
+
+            while(nums[currIndex] > 0 && nums[currIndex] <= nums.length && !isIntegerAtCorrectIndex(nums, currIndex)) {
+                int correctLocForCurrValue = nums[currIndex] - 1;
+                swap(nums, correctLocForCurrValue, currIndex);
+            }
+
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
     public int firstMissingPositive(int [] nums) {
         // put the integers in its corresponding indexes.
         // then traverse the modified array to check which index is missing the number
+
         for(int i = 0; i < nums.length; i++) {
+            // we are trying to find the right number for this index and hence we swap curr value at i
+            // with the value from the correct index for current value.
             while(nums[i] > 0 && nums[i] <= nums.length && !isIntegerAtItsIndex(nums, i)) {
                 // nums not at its correct location.
-                int currIndex = i;
+                //int currIndex = i;
                 int correctLoc = nums[i] - 1;
-                swap(nums, currIndex, correctLoc);
+                swap(nums, i, correctLoc);
             }
         }
 
@@ -48,6 +73,21 @@ public class FirstMissingPositive {
         ans = solve.firstMissingPositive(new int[]{3,4,-1,1});
         //[3,4,-1,1]
         System.out.println("Ans: " + ans);
+        ans = solve.firstMissingPostiveRevision(new int[]{3,4,-1,1});
+        //[3,4,-1,1]
+        System.out.println("Ans: " + ans);
 
     }
+
+
+
+    private boolean isIntegerAtCorrectIndex(int [] nums, int indexToBeChecked) {
+        int intToBeChecked = nums[indexToBeChecked];
+        int desiredLoc = intToBeChecked - 1;
+        return nums[desiredLoc] == intToBeChecked;
+    }
+
+
+
+
 }
